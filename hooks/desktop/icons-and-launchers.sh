d2716d9030ecf470271f0aae0b4e7239f3c91974
arshavirm <arshavir2468@gmail.com>
2026-09-23 18:41:08 +0330

    script fix

--- hooks/desktop/icons-and-launchers.sh
+++ hooks/desktop/icons-and-launchers.sh
@@ -2,4 +2,4 @@
 set -eu
 cp -a /.temp_assets/persisos-plasma-theme/usr/share/icons /usr/share/
 cp -a /.temp_assets/persisos-plasma-theme/usr/share/applications /usr/share/
-rm /usr/share/calamares.desktop
+rm /usr/share/applications/calamares.desktop
