eae003695943f2798f0abd0493543f55acc87850
arshavirm <arshavir2468@gmail.com>
2026-09-23 18:22:58 +0330

    update

--- hooks/desktop/icons-and-launchers.sh
+++ hooks/desktop/icons-and-launchers.sh
@@ -2,3 +2,4 @@
 set -eu
 cp -a /.temp_assets/persisos-plasma-theme/usr/share/icons /usr/share/
 cp -a /.temp_assets/persisos-plasma-theme/usr/share/applications /usr/share/
+rm /usr/share/calamares.desktop
